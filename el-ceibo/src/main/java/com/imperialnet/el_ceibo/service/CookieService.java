829641ee6f3992e2de228722019d68d3e49c51c9
MichaelIllescas <joni.illes@hotmail.com>
2025-02-01 14:31:35 -0300

    Version 1.0.1

--- el-ceibo/src/main/java/com/imperialnet/el_ceibo/service/CookieService.java
+++ el-ceibo/src/main/java/com/imperialnet/el_ceibo/service/CookieService.java
@@ -9,10 +9,10 @@ public class CookieService {
     public ResponseCookie createAuthCookie(String jwt) {
         return ResponseCookie.from("authToken", jwt)
                 .httpOnly(false)  // Permitir acceso desde el frontend
-                .secure(true)    // Cambiar a true si usas HTTPS
-                .path("/")        // Disponible en toda la aplicación
-                // .domain("https://elceibo.imperial-net.com")  // Dominio para compartir la cookie entre subdominios
-                .sameSite("Lax")  // Política SameSite para evitar CSRF
+                .secure(true)    // Solo se envía en HTTPS
+                .sameSite("None")  // Permite cross-origin
+                .domain(".imperial-net.com")  // Permite compartir entre subdominios
+                .path("/")
                 .maxAge(3600)     // Duración de la cookie en segundos (1 hora)
                 .build();
     }
